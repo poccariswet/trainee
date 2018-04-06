e56621fac9430a19dadca5f6166371597323f3c8
soeyusuke <s1240231@gmail.com>
2018-04-06 15:34:49 +0900

    [ADD] bgcolor

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,71 @@
 <template>
   <div id="app">
     <h1 class="title"> demo app </h1>
-    <div class="select-block">
-    <div class="block">
-      <el-date-picker
-        v-model="value1"
-        type="date"
-        placeholder="Pick a day"
-        :picker-options="picupOption">
-      </el-date-picker>
-    </div>
+    <div class="backcolor">
+      <div class="select-block">
+        <div class="block">
+          <el-date-picker
+            v-model="value1"
+            type="date"
+            placeholder="Pick a day"
+            :picker-options="picupOption">
+          </el-date-picker>
+        </div>
 
-    <div class="select">
-      <el-select v-model="value2" clearable placeholder="Select">
-        <el-option
-          v-for="item in options1"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-    </div>
+        <div class="select">
+          <el-select v-model="value2" clearable placeholder="Select">
+            <el-option
+              v-for="item in options1"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
 
-    <div class="select">
-      <el-select v-model="value3" clearable placeholder="Select">
-        <el-option
-          v-for="item in options1"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-    </div>
+        <div class="select">
+          <el-select v-model="value3" clearable placeholder="Select">
+            <el-option
+              v-for="item in options1"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
 
-    <div class="select">
-      <el-select v-model="value4" clearable placeholder="Select">
-        <el-option
-          v-for="item in options1"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-    </div>
+        <div class="select">
+          <el-select v-model="value4" clearable placeholder="Select">
+            <el-option
+              v-for="item in options1"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
 
-    <div class="select">
-      <el-select v-model="value5" clearable placeholder="Select">
-        <el-option
-          v-for="item in options1"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-        </el-option>
-      </el-select>
-    </div>
-    </div>
+        <div class="select">
+          <el-select v-model="value5" clearable placeholder="Select">
+            <el-option
+              v-for="item in options1"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
 
+      </div>
+      <div class="button">
+        <el-button type="primary" icon="el-icon-search" @click="Search">Search</el-button>
+      </div>
+    </div>
     <h2 class="val"> {{ value1 }} </h2>
     <h2 class="val"> {{ value2 }} </h2>
     <h2 class="val"> {{ value3 }} </h2>
     <h2 class="val"> {{ value4 }} </h2>
     <h2 class="val"> {{ value5 }} </h2>
-
   </div>
 </template>
 
@@ -96,13 +100,17 @@ export default {
       value4: '',
       value5: ''
     }
+  },
+  methods: {
+    Search () {
+
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  background: #F5F5F5;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -114,13 +122,18 @@ h1.title {
   color: #8492A6;
 }
 
+.backcolor {
+  background: #F5F5F5;
+}
+
 .select-block {
+  height: 100%;
   padding: 5%;
 }
 
 .block {
   margin-top: auto;
-  margin-left: 5%;
+  margin-left: 6%;
   float:left;
 }
 
@@ -131,11 +144,15 @@ h1.title {
 }
 
 h2.val {
-  margin-top: auto;
+  margin-top: 5px;
   padding-top: 5px;
   color: #8492A6;
   text-align: center;
 }
 
+.button {
+  margin-top: auto;
+  text-align: center;
+}
 </style>
 
